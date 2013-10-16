f05db660ae0f93ecd554f6e34b7fad8da709cfa3
Paramaribo <darkkagan@gmail.com>
2013-10-16 21:08:07 +0300

    start working!

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/voronina-store/admin/');
+define('HTTP_CATALOG', 'http://localhost/voronina-store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/voronina-store/admin/');
+define('HTTPS_CATALOG', 'http://localhost/voronina-store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\voronina-store/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\voronina-store/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\voronina-store/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\voronina-store/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\voronina-store/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\voronina-store/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\voronina-store/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\voronina-store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\voronina-store/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\voronina-store/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\voronina-store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'administrator');
+define('DB_PASSWORD', 'legendary23');
+define('DB_DATABASE', 'voronina_store');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/voronina-store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/voronina-store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\voronina-store/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\voronina-store/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\voronina-store/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\voronina-store/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\voronina-store/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\voronina-store/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\voronina-store/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\voronina-store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\voronina-store/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\voronina-store/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'administrator');
+define('DB_PASSWORD', 'legendary23');
+define('DB_DATABASE', 'voronina_store');
+define('DB_PREFIX', 'oc_');
+?>
